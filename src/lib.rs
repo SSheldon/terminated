2d06855ddc79c7c3018fccee2014080aeca1c8cb
Steven Sheldon <steven@sasheldon.com>
2017-04-30 11:33:56 -0700

    Document crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,30 @@
+/*!
+This crate provides types for representing NUL-terminated UTF8 strings.
+
+The `NulTerminatedStr` type is useful when interacting with C APIs that
+require/guarantee UTF8 encoding. Rust has great support for dealing with UTF8,
+but C strings require a NUL terminator which Rust's `str` and `String` don't have.
+
+```
+# #[macro_use] extern crate nul;
+# fn main() {
+let s = ntstr!("Hello, World!");
+
+// You can use Rust's normal string operations
+assert_eq!(s.find("World"), Some(7));
+
+// And pass it to C since it's NUL-terminated
+let ptr = s.as_ptr();
+# }
+```
+
+# CStr vs NulTerminatedStr
+
+The standard library does provide the `CStr` type that is NUL-terminated,
+but it does not use any specific encoding. It's therefore insufficient
+if your input needs to be both NUL-terminated and UTF8 encoded.
+*/
+
 #![no_std]
 
 use core::fmt;
